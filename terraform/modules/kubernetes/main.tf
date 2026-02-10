8b60335288d09eeba99d31bc26ed1ed1d81cd363
szuryuu <ilhamdzaky2007@gmail.com>
2026-02-10 14:37:08 +0700

    fix: gitignore and upgrade vm size

--- terraform/modules/kubernetes/main.tf
+++ terraform/modules/kubernetes/main.tf
@@ -7,7 +7,7 @@ resource "azurerm_kubernetes_cluster" "main" {
   default_node_pool {
     name           = "default"
     node_count     = 1
-    vm_size        = "Standard_B1s"
+    vm_size        = "Standard_B2s"
     vnet_subnet_id = var.subnet_id
   }
 
